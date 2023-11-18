10337edcefe09114a7368a29f949055903069af8
azamat <azamatabdurashitov0@gmail.com>
2023-11-18 23:09:42 +0500

    start

---
+++ main/models.py
@@ -0,0 +1,50 @@
+from django.db import models
+
+class Home(models.Model):
+    title = models.CharField(max_length=255)
+    sub_title = models.CharField(max_length=255)
+
+    def __str__(self) -> str:
+        return self.title
+    
+    class Meta:
+        verbose_name = 'Home'
+        verbose_name_plural = 'Homes'
+
+class About(models.Model):
+    title = models.CharField(max_length=255)
+    sub_title = models.CharField(max_length=255)
+
+    def __str__(self) -> str:
+        return self.title
+    
+    class Meta:
+        verbose_name = 'About'
+        verbose_name_plural = 'Abouts'
+
+class Services(models.Model):
+    title = models.CharField(max_length=255)
+    sub_title = models.CharField(max_length=255)
+    icon = models.ImageField(upload_to='Services/detail_picture/%Y/%m/%d', blank=True)
+    icon_title = models.CharField(max_length=255, blank=True)
+    icon_sub_title = models.CharField(max_length=255, blank=True)
+
+    def __str__(self) -> str:
+        return self.title
+    
+    class Meta:
+        verbose_name = 'Service'
+        verbose_name_plural = 'Services'
+
+class Contact(models.Model):
+    title = models.CharField(max_length=255)
+    photo = models.ImageField(upload_to='Services/detail_picture/%Y/%m/%d', blank=True)
+
+    def __str__(self) -> str:
+        return self.title
+    
+    class Meta:
+        verbose_name = 'Contact'
+        verbose_name_plural = 'COntacts'
+
+
